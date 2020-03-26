ad46b1757e26a5bb9662d8e1a73e7d3d255c010f
Samuel Idehen <samuelidehenlion@gmail.com>
2020-03-25 21:18:16 -0400

    --handling erroeMessage and loading

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -74,8 +74,15 @@ class Auth extends Component {
     if (this.props.loading) {
       form = <Spinner />;
     }
+
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage = <p>{this.props.error.message}</p>;
+    }
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success"> SUBMIT</Button>
@@ -89,7 +96,7 @@ class Auth extends Component {
 }
 
 const mapStateToProps = state => {
-  return { loading: state.auth.loading };
+  return { loading: state.auth.loading, error: state.auth.error };
 };
 
 const mapDispatchToProps = dispatch => {
