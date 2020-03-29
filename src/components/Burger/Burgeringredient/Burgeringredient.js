98719d9e17dfb7a96d98c7702110141f25bab949
Samuel Idehen <samuelidehenlion@gmail.com>
2020-03-28 21:56:37 -0400

    Adding seeds fix

--- src/components/Burger/Burgeringredient/Burgeringredient.js
+++ src/components/Burger/Burgeringredient/Burgeringredient.js
@@ -12,8 +12,8 @@ export default function Burgeringredient(props) {
     case "bread-top":
       ingredient = (
         <div className={classes.BreadTop}>
-          <div className={classes.Seed1}></div>
-          <div className={classes.Seed2}></div>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
         </div>
       );
       break;
